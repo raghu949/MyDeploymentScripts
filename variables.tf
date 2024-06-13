d37fb4311537a582616b5d4e36a798e4ed6c9042
Raghu Ram N <133505947+raghu949@users.noreply.github.com>
2024-06-13 21:08:40 +0530

    Create variables.tf
    Adding the file varaibles.tf

---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "aws-region" {
+  description = "The AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+
+
+
+variable "instance-ami" {
+  description = "The AMI (Amazon Machine Image) that identifies the instance"
+  type        = string
+  default     = "ami-0f58b397bc5c1f2e8"
+}
+
+variable "instance-type" {
+  description = "The instance type to be used"
+  type        = string
+  default     = "t2.micro"
+}
+
+
+variable "iam-role-name" {
+  description = "The IAM role to assign to the instance"
+  type        = string
+  default     = ""
+}
+
+variable "instance-associate-public-ip" {
+  description = "Defines if the EC2 instance has a public IP address."
+  type        = string
+  default     = "true"
+}
+
+
+variable "vpc-cidr-block" {
+  description = "The CIDR block to associate to the VPC"
+  type        = string
+  default     = "150.0.0.0/16"
+}
+
+variable "subnet-cidr-block" {
+  description = "The CIDR block to associate to the subnet"
+  type        = string
+  default     = "150.0.1.0/24"
+}
+
+variable "ig-tag-name" {
+  description = "The name to apply to the Internet gateway tag"
+  type        = string
+  default     = "aws-ig-created-with-terraform"
+}
+
+variable "subnet-tag-name" {
+  description = "The Name to apply to the VPN"
+  type        = string
+  default     = "VPN-created-with-terraform"
+}
+
+variable "sg-tag-name" {
+  description = "The Name to apply to the security group"
+  type        = string
+  default     = "SG-created-with-terraform"
+}
